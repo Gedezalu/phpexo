2cff003c92401f9ab636fa5c368206a4b7263539
Gedezalu <72193750+Gedezalu@users.noreply.github.com>
2020-11-26 14:16:47 +0100

    test

--- html/page2.php
+++ html/page2.php
@@ -1,24 +1,32 @@
 <h2> Les Dernières News du JDR</h2>
         <h3> Un Jeu de rôle dans l'univers de Batman en préparation</h3>
-        <img src="Actu1.jpg" alt="illustartion article">
+            
+                <img src="Actu1.jpg" alt="illustration article">
+
         <p>Après avoir réalisé deux campagnes avec un fort succès pour le jeu de plateau Batman : Gotham City Chronicles, l'éditeur français Monolith compte poursuivre son exploration de l'univers du Chevalier Noir avec un autre pan ludique, qui prend un essor de plus en plus important ces dernières années : le jeu de rôle...<a href="http://www.comicsblog.fr/38171-Monolith_preepare_un_jeu_de_reole_dans_lunivers_Batman_Gotham_City_Chronicles_RPG">"Lire la suite"</a> </p>
         <div class="finparaph">
             <img src="para2.png" alt="logo de dés à 20 faces">
             </div>
             <h3> Présentation : Adventures in Austerion </h3>
-        <img src="Actu4.jpeg" alt="illustartion article">
+        
+                <img src="Actu4.jpeg" alt="illustration article">
+      
         <p>Les Ribauds sont envoyés dans une vieille tombe scellée où, jadis, se seraient tenues des réunions secrètes entre sorciers et vampires, et même sorciers-vampires (ou vampires-sorciers, selon la longueur des canines). Pour cette exploration périlleuse, Jacquel le Rat des tunnels est accompagné de Rokia, un Fléau des arcanes capable d’user de magie comme de discrétion, de Souleyman, un Conciliateur impérieux, un négociateur qu’il ne faut pas contrarier, de Mo’omma, une Lutteuse kizarienne, dont la puissance se dispute à l’agilité, et d’Izbàn, un Maraudeur des abîmes à qui le combat au fond d’un Donjon ne fait pas peur. Le groupe s’avance vers son objectif et les premiers ennuis commencent, à vous maintenant de continuer l’histoire ...<a href="https://www.trictrac.net/actus/adventures-in-austerion-1">"Lire la suite"</a> </p>
         <div class="finparaph">
             <img src="para2.png" alt="logo de dés à 20 faces">
             </div>
             <h3>Critique : Club </h3>
-            <img src="Actu2.jpg" alt="illustartion article">
+          
+                    <img src="Actu2.jpg" alt="illustration article">
+               
         <p>Dans le jeu de rôle le CLUB chaque joueur incarne un monstre de son choix parmi un large éventail de créatures de la littérature classique d’horreur ou des contes modernes. Tous sont membres du CLUB, un groupement d’agents exceptionnels luttant contre les forces du mal tout autour du globe, de nos jours...<a href="http://www.unificationfrance.com/article64463.html">"Lire la suite"</a></p>
         <div class="finparaph">
         <img src="para2.png" alt="logo de dés à 20 faces">
         </div>
         <h3>Critique : Alien </h3>
-        <img src="Actu3.jpg" alt="illustartion article">
+ 
+             <img src="Actu3.jpg" alt="illustration article"> 
+   
         <p> Voici le Kit de démarrage du jeu de rôle officiel ALIEN, un univers d’horreur bien réelle et de frénésie corporatiste, où les êtres synthétiques se prennent pour Dieu tandis que les camionneurs et les marines spatiaux deviennent les hôtes de créatures macabres tout juste nées. C’est un univers dur et impitoyable, et vous n’êtes qu’un pion sacrifiable....<a href="http://www.unificationfrance.com/article64333.html">"Lire la suite"</a> </p>
             <div class="finparaph">
         <img src="para2.png" alt="logo de dés à 20 faces">
