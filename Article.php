f81d1ac889a40d0bd2db942e3a5bb4779592d787
Gedezalu <72193750+Gedezalu@users.noreply.github.com>
2020-11-27 11:15:06 +0100

    Boucle foreach

--- Article.php
+++ Article.php
@@ -1,25 +1,25 @@
 <?php $article1 =[
     "titre" => " Un Jeu de rôle dans l'univers de Batman en préparation",
     "article" => "Après avoir réalisé deux campagnes avec un fort succès pour le jeu de plateau Batman : Gotham City Chronicles, l'éditeur français Monolith compte poursuivre son exploration de l'univers du Chevalier Noir avec un autre pan ludique, qui prend un essor de plus en plus important ces dernières années : le jeu de rôle...",
-    "lien" => "http://www.comicsblog.fr/38171-Monolith_preepare_un_jeu_de_reole_dans_lunivers_Batman_Gotham_City_Chronicles_RPG"
+    "lien" => "http://www.comicsblog.fr/38171-Monolith_preepare_un_jeu_de_reole_dans_lunivers_Batman_Gotham_City_Chronicles_RPG",
     "image" => "Actu1.jpg",
 ];
 $article2 =[
     "titre" => " Présentation : Adventures in Austerion",
     "article" => "Les Ribauds sont envoyés dans une vieille tombe scellée où, jadis, se seraient tenues des réunions secrètes entre sorciers et vampires, et même sorciers-vampires (ou vampires-sorciers, selon la longueur des canines). Pour cette exploration périlleuse, Jacquel le Rat des tunnels est accompagné de Rokia, un Fléau des arcanes capable d’user de magie comme de discrétion, de Souleyman, un Conciliateur impérieux, un négociateur qu’il ne faut pas contrarier, de Mo’omma, une Lutteuse kizarienne, dont la puissance se dispute à l’agilité, et d’Izbàn, un Maraudeur des abîmes à qui le combat au fond d’un Donjon ne fait pas peur. Le groupe s’avance vers son objectif et les premiers ennuis commencent, à vous maintenant de continuer l’histoire ...",
-    "lien" => "https://www.trictrac.net/actus/adventures-in-austerion-1"
+    "lien" => "https://www.trictrac.net/actus/adventures-in-austerion-1",
     "image" => "Actu4.jpeg",
 ];
 $article3 =[
     "titre" => " Critique : Club ",
     "article" => "Dans le jeu de rôle le CLUB chaque joueur incarne un monstre de son choix parmi un large éventail de créatures de la littérature classique d’horreur ou des contes modernes. Tous sont membres du CLUB, un groupement d’agents exceptionnels luttant contre les forces du mal tout autour du globe, de nos jours...",
-    "lien" => "http://www.unificationfrance.com/article64463.html"
+    "lien" => "http://www.unificationfrance.com/article64463.html",
     "image" => "Actu2.jpg",
 ];
 $article4 =[
     "titre" => " Critique : Alien ",
     "article" => "Voici le Kit de démarrage du jeu de rôle officiel ALIEN, un univers d’horreur bien réelle et de frénésie corporatiste, où les êtres synthétiques se prennent pour Dieu tandis que les camionneurs et les marines spatiaux deviennent les hôtes de créatures macabres tout juste nées. C’est un univers dur et impitoyable, et vous n’êtes qu’un pion sacrifiable...",
-    "lien" => "http://www.unificationfrance.com/article64333.html"
+    "lien" => "http://www.unificationfrance.com/article64333.html",
     "image" => "Actu3.jpg",
 ];
  $articles = [ $article1, $article2, $article3, $article4];
--- html/page2.php
+++ html/page2.php
@@ -1,56 +1,19 @@
 <?php
 require_once "Article.php";
-/* Test non concluant
- $len = $articles.length;
-
-for ($i=0, i<$len , i++)
-{ 
-
-   echo <h3> <?php require $articles[i].titre ?> </h3>;
-   echo <p class="Cadreactu"><?php require $articles[i].article ?>;
-   echo <a href ="<?php require $articles[i].lien ?>" class="lienarticle">"Lire la suite"</a> ;
-   echo <img src="<?php require $articles[i].image ?>";
-   echo <div class="finparaph">;
-   echo <img src="para2.png" alt="logo de dés à 20 faces">;
-   echo  </div>;
-} */
-
-?>
-
+$html ="";
+/*$len = sizeof($articles);
+for ($i = 0; $i < $len ; $i++) {
+    $html .= "<h3>" . $articles[$i]["titre"] ." </h3>"; //<  ou $html = $html.html  
+    $html .= "<img src=\"" . $articles[$i]["image"] . "\"\" alt=\"illustration article\">";
+    $html .= "<p class=\"Cadreactu\">" . $articles[$i]["article"] . "</p>";
+    $html .= "<a href=\""  . $articles[$i]["lien"] ."\" class=\"lienarticle\">\"Lire la suite\"</a> <div class=\"finparaph\"> <img src=\"para2.png\" alt=\"logo de dés à 20 faces\"> </div>";
+}*/
+foreach($articles as $index => $article) {
+    $html .= "<h3> Article n°" . ($index + 1) . " " . $article["titre"] .  "</h3>";
+    $html .= "<img src=\"" . $article["image"] . "\"\" alt=\"illustration article\">";
+    $html .= "<p class=\"Cadreactu\">" . $article["article"] . "</p>";
+    $html .= "<a href=\""  . $article["lien"] ."\" class=\"lienarticle\">\"Lire la suite\"</a> <div class=\"finparaph\"> <img src=\"para2.png\" alt=\"logo de dés à 20 faces\"> </div>";
+} 
+?> 
 <h2> Les Dernières News du JDR</h2>
-        <h3> Un Jeu de rôle dans l'univers de Batman en préparation</h3>
-            
-                <img src="Actu1.jpg" alt="illustration article">
-
-        <p class="Cadreactu">Après avoir réalisé deux campagnes avec un fort succès pour le jeu de plateau Batman : Gotham City Chronicles, l'éditeur français Monolith compte poursuivre son exploration de l'univers du Chevalier Noir avec un autre pan ludique, qui prend un essor de plus en plus important ces dernières années : le jeu de rôle...</p>
-        <a href="http://www.comicsblog.fr/38171-Monolith_preepare_un_jeu_de_reole_dans_lunivers_Batman_Gotham_City_Chronicles_RPG" class="lienarticle">"Lire la suite"</a> 
-        <div class="finparaph">
-            <img src="para2.png" alt="logo de dés à 20 faces">
-            </div>
-            <h3> Présentation : Adventures in Austerion </h3>
-        
-                <img src="Actu4.jpeg" alt="illustration article">
-      
-        <p class="Cadreactu">Les Ribauds sont envoyés dans une vieille tombe scellée où, jadis, se seraient tenues des réunions secrètes entre sorciers et vampires, et même sorciers-vampires (ou vampires-sorciers, selon la longueur des canines). Pour cette exploration périlleuse, Jacquel le Rat des tunnels est accompagné de Rokia, un Fléau des arcanes capable d’user de magie comme de discrétion, de Souleyman, un Conciliateur impérieux, un négociateur qu’il ne faut pas contrarier, de Mo’omma, une Lutteuse kizarienne, dont la puissance se dispute à l’agilité, et d’Izbàn, un Maraudeur des abîmes à qui le combat au fond d’un Donjon ne fait pas peur. Le groupe s’avance vers son objectif et les premiers ennuis commencent, à vous maintenant de continuer l’histoire ...</p>
-        <a href="https://www.trictrac.net/actus/adventures-in-austerion-1" class="lienarticle">"Lire la suite"</a> 
-        <div class="finparaph">
-            <img src="para2.png" alt="logo de dés à 20 faces">
-            </div>
-            <h3>Critique : Club </h3>
-          
-                    <img src="Actu2.jpg" alt="illustration article">
-               
-        <p class="Cadreactu">Dans le jeu de rôle le CLUB chaque joueur incarne un monstre de son choix parmi un large éventail de créatures de la littérature classique d’horreur ou des contes modernes. Tous sont membres du CLUB, un groupement d’agents exceptionnels luttant contre les forces du mal tout autour du globe, de nos jours...</p>
-        <a href="http://www.unificationfrance.com/article64463.html" class="lienarticle">"Lire la suite"</a>
-        <div class="finparaph">
-        <img src="para2.png" alt="logo de dés à 20 faces">
-        </div>
-        <h3>Critique : Alien </h3>
- 
-             <img src="Actu3.jpg" alt="illustration article"> 
-   
-        <p class="Cadreactu"> Voici le Kit de démarrage du jeu de rôle officiel ALIEN, un univers d’horreur bien réelle et de frénésie corporatiste, où les êtres synthétiques se prennent pour Dieu tandis que les camionneurs et les marines spatiaux deviennent les hôtes de créatures macabres tout juste nées. C’est un univers dur et impitoyable, et vous n’êtes qu’un pion sacrifiable.... </p>
-        <a href="http://www.unificationfrance.com/article64333.html" class="lienarticle">"Lire la suite"</a>
-            <div class="finparaph">
-        <img src="para2.png" alt="logo de dés à 20 faces">
-            </div>
+<?= $html ?>
